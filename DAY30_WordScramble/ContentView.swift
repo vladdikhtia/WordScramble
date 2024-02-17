66dbf91adfeafd37d8259d145e1f1e9e71a6a272
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-02-17 16:58:37 +0100

    Initial Commit

---
+++ DAY30_WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DAY30_WordScramble
+//
+//  Created by Vladyslav Dikhtiaruk on 17/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DAY30_WordScramble/DAY30_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAY30_WordScrambleApp.swift
+//  DAY30_WordScramble
+//
+//  Created by Vladyslav Dikhtiaruk on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DAY30_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
